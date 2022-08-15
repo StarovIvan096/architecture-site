da10d5921134ccdd1b366dba6c7418ca5e77f5af
Your Name <you@example.com>
2022-08-15 16:42:46 +0300

    добавил 2 между собой связанных слайдера во 2 секции

--- script.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector('body'),
-    toggle = document.querySelector('.change-theme');
-
-toggle.addEventListener('click', ()=> {
-    body.classList.toggle('light');
-    body.classList.toggle('dark');
-});
